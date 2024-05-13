c9590bc1e04fe497f7eabee5f5beec3cfd7d636b
Mehmet Can Yavuz <myavuz@fairfax.radiology.ucsf.edu>
2024-05-13 06:58:51 -0700

    the working algorithm for VCL

--- simclr.py
+++ simclr.py
@@ -8,119 +8,45 @@ from torch.nn import Identity
 from torchvision.models import resnet50
 
 from lightly.loss.ntx_ent_loss import NTXentLoss
+from lightly.models.modules import SimCLRProjectionHead
 from lightly.models.utils import get_weight_decay_parameters
 from lightly.transforms import SimCLRTransform
 from lightly.utils.benchmarking import OnlineLinearClassifier
 from lightly.utils.lars import LARS
 from lightly.utils.scheduler import CosineWarmupScheduler
 
-import torch.nn as nn
-import torch.nn.functional as F
 
-import torch.distributions as tdist
-
-class vloss(nn.Module):
-    def __init__(self):
-        super(vloss,self).__init__()
-
-    def l1(self, x, y):
-        return x - y
-
-    def l2(self, x, y):
-        return (x - y).square()
-
-    def jsd_gaussian(self, p_mean, q_mean, m_mean, logVar_):
-        logVarp = logVar_[:,0,:]
-        logVarq = logVar_[:,1,:]
-        
-        std = 0.5 * ((logVarp /2).exp() + (logVarq /2).exp())
-        logVarm = std.log() * 2
-        
-        term_1 = 0.5 * ((logVarm - logVarq) + (logVarm - logVarq))
-        term_2 = ((m_mean - p_mean).pow(2) + (m_mean - q_mean).pow(2)).div(2 * logVarm.exp())
-
-        
-        # Calculate JSD as the average of the KL divergences
-        jsd = term_1 + term_2
-
-        return jsd  
-    
-    def forward(self, mu, logVar):
-        mp = mu[:,0,:]
-        mq = mu[:,1,:]
-        mm = 0.5 * (mp + mq)
-
-        jsd = self.jsd_gaussian(mp, mq, mm, logVar).mean()
-        norm_ = - 0.5 * (1 + logVar - mu.pow(2) - logVar.exp()).mean()
-        return jsd + 0.002 * norm_
-
-class Projector(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super(Projector, self).__init__()
-        self.interpreter = nn.Sequential(nn.Linear(input_dim, hidden_dim),
-                    nn.BatchNorm1d(hidden_dim),
-                    nn.ReLU(inplace=True),
-                    nn.Linear(hidden_dim, hidden_dim),
-                    nn.BatchNorm1d(hidden_dim),
-                    nn.ReLU(inplace=True))
-        self.fc_mu = nn.Linear(hidden_dim, output_dim)
-        self.fc_var = nn.Linear(hidden_dim, output_dim)
-
-    def reparameterize(self, mu, logVar):
-        std = logVar.mul(0.5).exp_()
-        eps = torch.randn_like(std)
-        return eps * std + mu      
-        
-    def forward(self, x):
-        x = self.interpreter(x)
-        mu = self.fc_mu(x)
-        logVar = self.fc_var(x)
-        rp = self.reparameterize(mu, logVar)
-        return rp, mu, logVar
-
-class VCL(LightningModule):
-    def __init__(self, batch_size_per_device: int, num_classes: int, num_gpus: int) -> None:
+class SimCLR(LightningModule):
+    def __init__(self, batch_size_per_device: int, num_classes: int) -> None:
         super().__init__()
         self.save_hyperparameters()
         self.batch_size_per_device = batch_size_per_device
-        self.num_gpus = num_gpus
 
         resnet = resnet50()
         resnet.fc = Identity()  # Ignore classification head
         self.backbone = resnet
-        
-        feature_dim = 2048
-        self.projection_head = Projector(feature_dim, feature_dim, 128)
-        
-        self.criterion = NTXentLoss(temperature=0.07, gather_distributed=True)
-        self.vloss = vloss()
+        self.projection_head = SimCLRProjectionHead()
+        self.criterion = NTXentLoss(temperature=0.1, gather_distributed=True)
 
         self.online_classifier = OnlineLinearClassifier(num_classes=num_classes)
 
-    def forward(self, x):
-        x = self.backbone(x).flatten(start_dim=1)
-        z, mu, logVar = self.projection_head(x)
-        return x, z, mu, logVar
+    def forward(self, x: Tensor) -> Tensor:
+        return self.backbone(x)
 
     def training_step(
         self, batch: Tuple[List[Tensor], Tensor, List[str]], batch_idx: int
     ) -> Tensor:
-        (x0, x1), targets = batch[0], batch[1]
-        feature1, z0, mu0, logVar0 = self.forward(x0)
-        feature2, z1, mu1, logVar1 = self.forward(x1)
-        features = torch.cat([feature1, feature2])
-
-        mu = torch.cat((mu0.unsqueeze(1), mu1.unsqueeze(1)),1)
-        logVar = torch.cat((logVar0.unsqueeze(1), logVar1.unsqueeze(1)),1)
-
-        loss = self.criterion(z0, z1) + self.vloss(mu, logVar)        
-
+        views, targets = batch[0], batch[1]
+        features = self.forward(torch.cat(views)).flatten(start_dim=1)
+        z = self.projection_head(features)
+        z0, z1 = z.chunk(len(views))
+        loss = self.criterion(z0, z1)
         self.log(
             "train_loss", loss, prog_bar=True, sync_dist=True, batch_size=len(targets)
         )
 
         cls_loss, cls_log = self.online_classifier.training_step(
-            (features.detach(), targets.repeat(len(batch[0]))), batch_idx
+            (features.detach(), targets.repeat(len(views))), batch_idx
         )
         self.log_dict(cls_log, sync_dist=True, batch_size=len(targets))
         return loss + cls_loss
@@ -129,7 +55,7 @@ class VCL(LightningModule):
         self, batch: Tuple[Tensor, Tensor, List[str]], batch_idx: int
     ) -> Tensor:
         images, targets = batch[0], batch[1]
-        features, _, _, _ = self.forward(images)        
+        features = self.forward(images).flatten(start_dim=1)
         cls_loss, cls_log = self.online_classifier.validation_step(
             (features.detach(), targets), batch_idx
         )
